edd0cb2578c788ab244f0ff490123fb8e5de1a6a
adixb <aditya.bhattacharjee706@gmail.com>
2024-08-02 14:23:05 +0530

    upload functionality done

--- src/Components/WebcamCapture.jsx
+++ src/Components/WebcamCapture.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Webcam from 'react-webcam';
 
 function WebcamCapture() {
@@ -8,8 +8,32 @@ function WebcamCapture() {
     facingMode: "user",
   };
 
+  const [imageWebCam, setImageWebCam] = useState(null);
+  const [showImage, setShowImage] = useState(false);
+
+  const handleCapture = () => {
+    setShowImage(true);
+    setTimeout(() => {
+      setShowImage(false);
+      setImageWebCam(null);
+    },10000);
+  };
+
   return (
-    <>
+    <div className="relative flex flex-col items-center justify-center">
+      {showImage && imageWebCam && (
+        <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75 z-50">
+          <div className="relative">
+            <img src={imageWebCam} alt="Captured" className="rounded-xl" />
+          </div>
+          <button
+                className="my-12  text-black bg-white p-2 font-semibold shadow-xl rounded-xl text-sm hover:text-white hover:bg-black"
+                
+              >
+                UPLOAD
+              </button>
+        </div>
+      )}
       <Webcam
         audio={false}
         height={500}
@@ -19,20 +43,19 @@ function WebcamCapture() {
         className='p-2 rounded-xl'
       >
         {({ getScreenshot }) => (
-          <div>
-            <button
-              onClick={() => {
-                const imageSrc = getScreenshot();
-                console.log(imageSrc); // Do something with the captured image
-              }}
-              className='my-12  text-black bg-white p-2 font-semibold shadow-xl rounded-xl text-sm hover:text-white hover:bg-black'
-            >
-              Capture Photo
-            </button>
-          </div>
+          <button
+            onClick={() => {
+              const imageSrc = getScreenshot();
+              setImageWebCam(imageSrc);
+              handleCapture();
+            }}
+            className='my-12 text-black bg-white p-2 font-semibold shadow-xl rounded-xl text-sm hover:text-white hover:bg-black'
+          >
+            Capture Photo
+          </button>
         )}
       </Webcam>
-    </>
+    </div>
   );
 }
 
--- src/Pages/SolvePage.jsx
+++ src/Pages/SolvePage.jsx
@@ -1,36 +1,66 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-import WebcamCapture from '../Components/WebcamCapture'
-
-
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar";
+import WebcamCapture from "../Components/WebcamCapture";
 
 function SolvePage() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
 
+  const handleImageChange = (e) => {
+    const files = e.target.files[0];
 
+    if (files) {
+      setSelectedImage(files);
+    }
+
+    const readFile = new FileReader();
+    readFile.onloadend = () => {
+      setPreviewImage(readFile.result);
+    };
+    readFile.readAsDataURL(files);
+  };
+
+  const handleSubmitImage=()=>{
+    if(!selectedImage){
+    alert("Please Enter an Image !") ; 
+    return;
+    }
+    console.log("The Image has been uplaoded",selectedImage) ; 
+    
+  }
 
   return (
     <>
+      <div className="  w-screen h-screen">
+        <Navbar />
 
-    <div className="  w-screen h-screen">
-    <Navbar  />
-
-    <div className=" w-screen h-auto flex items-center justify-center p-2 ">
-      <div className=" m-2 w-1/2 border h-[80vh] mt-12 bg-yellow-400 flex flex-col items-center justify-center">
-      <p className='text-4xl my-5 text-black font-bold'>Upload a picture with your Web-Cam</p>
-        <WebcamCapture/>
-
-      </div>
-      <div className="w-1/2 border m-2 h-[80vh] p-2 flex items-center justify-center mt-12 bg-red-500">
-        <div className="flex flex-col items-center justify-around">
-          <p className='text-4xl text-white font-bold'>Upload Picture from your Device</p>
-          <button className='my-12  text-black bg-white p-2 font-semibold shadow-xl rounded-xl text-sm hover:text-white hover:bg-black'>UPLOAD</button>
+        <div className=" w-screen h-auto flex items-center justify-center p-2 ">
+          <div className=" m-2 w-1/2 border h-[80vh] mt-12 bg-yellow-400 flex flex-col items-center justify-center">
+            <p className="text-4xl my-5 text-black font-bold">
+              Upload a picture with your Web-Cam
+            </p>
+            <WebcamCapture />
+            
+          </div>
+          <div className="w-1/2 border m-2 h-[80vh] p-2 flex items-center justify-center mt-12 bg-red-500">
+            <div className="flex flex-col items-center justify-around">
+              <p className="text-4xl text-white font-bold">
+                Upload Picture from your Device
+              </p>
+              <input type="file" accept="image/*" onChange={handleImageChange} className="my-5 text-white font-bold" />
+              {previewImage && <img src={previewImage} alt="ImagePreview" style={{ width: '200px', height: '200px' }} />}
+              <button
+                className="my-12  text-black bg-white p-2 font-semibold shadow-xl rounded-xl text-sm hover:text-white hover:bg-black"
+                onClick={handleSubmitImage}
+              >
+                UPLOAD
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-
-    </div>
     </>
-  )
+  );
 }
 
-export default SolvePage ;
+export default SolvePage;
